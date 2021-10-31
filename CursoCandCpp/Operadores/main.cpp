22a6e68db1602291c9796fa0ffa422879a9752b0
natandovale <natandovale496@gmail.com>
2021-10-31 15:40:42 -0300

    Curso de C-Cpp

---
+++ CursoCandCpp/Operadores/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+	float resultado;
+    n1 = 10;
+    n2 = 20;
+    resultado = (float)n1 / n2;
+    
+    cout << resultado;
+    
+    return 0;       
+}
