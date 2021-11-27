0354ff7cb412035ea587841e5a45a3b242df493a
natanaeloliveira <natanaeloliveira@mobills.com.br>
2021-11-27 15:32:42 -0300

    Criando loop para while

--- JogoDeAdivinhacao/Adivinhacao.c
+++ JogoDeAdivinhacao/Adivinhacao.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 
+
 int main(){
 
     printf("****************************************\n");
     printf("*Bem vindo ao nosso jogo de adivinhacao*\n");
     printf("****************************************\n");
-
-    int numeroSecreto = 42;
+    
+    int numerosecreto = 42;
     int chute;
+    int tentativas = 0;
 
-    printf("Qual e o seu chute? \n");
-    scanf("%d", &chute);
+    while(1) {
 
-    printf("Seu chute foi %d \n", chute);
+        printf("Tentativa %d\n", tentativas+1);
+        printf("Qual e o seu chute? ");
 
-    if(chute == numeroSecreto){
-    	printf("Parabens você acertou!\n");
-    }else{
-    	printf("Você errou!");
-    }
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
+
+        if(chute < 0) {
+            printf("Voce nao pode chutar numeros negativos!\n");
+            continue;
+        }
+
+        int acertou = (chute == numerosecreto);
+        int maior = chute > numerosecreto;
+
+        if(acertou) {
+            printf("Parabens! Voce acertou!\n");
+            printf("Jogue de novo, voce e um bom jogador!\n");
+
+            break;
+        }
+        else if(maior) {
+            printf("Seu chute foi maior que o numero secreto!\n");
+        }else{
+            printf("Seu chute foi menor que o numero secreto!\n");
+        }
+        tentativas++;
+  }
+        printf("Fim de jogo!\n");
+        printf("Voce acertou em %d tentativas!", tentativas+1);
 }
