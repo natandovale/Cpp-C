d4cc7132c88bd71afc76086be54faa84be120ecc
natanaeloliveira <natanaeloliveira@mobills.com.br>
2021-12-12 14:39:01 -0300

    Assunto ponteiros

---
+++ CursoCandCpp/Ponteiros/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//ponteiros são variaveis que contêm o endereço de outra variável na memoria interna do pc
+void foo(int* n)
+{
+	*n = 20;
+}
+
+int main(int argc, char** argv)
+{
+	int var = 10;
+	//int* pvar;
+	//inicializando com o endereço da variavel (var) usando '&'
+	//pvar = &var;
+	//cout << *pvar << endl;
+	//*pvar = 20;
+	foo(&var);
+	cout << var << endl;
+	
+	return 0;
+}
