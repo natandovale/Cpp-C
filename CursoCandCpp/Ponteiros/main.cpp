dd150886e8cc575f9f6834ff5a39f3a5dc133715
natanaeloliveira <natanaeloliveira@mobills.com.br>
2021-12-27 08:59:47 -0300

    Ponteiros

--- CursoCandCpp/Ponteiros/main.cpp
+++ CursoCandCpp/Ponteiros/main.cpp
@@ -10,13 +10,15 @@ void foo(int* n)
 int main(int argc, char** argv)
 {
 	int var = 10;
+	// declara um ponteiro para um inteiro,um ponteiro para uma variável do tipo inteiro
 	//int* pvar;
 	//inicializando com o endereço da variavel (var) usando '&'
 	//pvar = &var;
 	//cout << *pvar << endl;
+	//O "*" acessa o conteudo da variavel apontada pelo ponteiro
 	//*pvar = 20;
 	foo(&var);
 	cout << var << endl;
-	
+
 	return 0;
 }
